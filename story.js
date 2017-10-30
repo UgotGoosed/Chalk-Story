df6d1b1d40a9ac2a9f596493f99b37bc969ed32e
Michael Ganschow <altern8universe@yahoo.com>
2017-10-30 18:06:40 -0500

    finished coloring the

--- story.js
+++ story.js
@@ -1,2 +1,13 @@
-const 
-console.log('hello');
+const chalk = require('chalk');
+const log = console.log;
+ 
+log(chalk.cyan('One day a ' + chalk.white.bgBlue('white cloud') + ' was floating in the air.'));
+log(chalk.green('This cloud became ' + chalk.underline.bold('sad') + ' when he saw how ' + chalk.yellow('yellow') + ' the grass was.'));
+log(chalk.magenta('He became ' + chalk.underline.bgWhite('so sad') + ' that he turned ' + chalk.gray('grey') + ' and started to cry. '));
+log(chalk.yellowBright('He ' + chalk.underline('cried') + ' and ' + chalk.underline('cried') + ' until he could not cry anymore.'));
+log(chalk.green('After he was done he talked to his friend, ' + chalk.white.bgYellow.bold('the Sun') + ', and went home.'));
+log(chalk.bgYellow.cyan('The Sun') + chalk.red(' told the cloud that he would take care of the grass for him,'));
+log(chalk.cyan('and when he came back the ' + chalk.yellow.underline('yellow grass') + ' would be ' + chalk.green.underline('green') + ' again.'));
+log(chalk.magenta('The next day the cloud came back and saw that the grass was ' + chalk.greenBright.underline('GREEN') + ' again!'));
+log(chalk.yellow('So the Cloud and the Sun relaxed and enjoyed the ' + chalk.blue.underline('beautiful') + chalk.green.underline(' green') + ' grass'));
+log(chalk.red.bold('all day.'));
